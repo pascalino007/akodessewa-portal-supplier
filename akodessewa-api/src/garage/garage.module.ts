802c08ae15370579a8255c0d370a5ad7a252d118
imaginov <134341734+pascalino007@users.noreply.github.com>
2026-02-20 11:48:31 +0000

    fisrt commit

---
+++ akodessewa-api/src/garage/garage.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { GarageService } from './garage.service';
+import { GarageController } from './garage.controller';
+
+@Module({
+  controllers: [GarageController],
+  providers: [GarageService],
+})
+export class GarageModule {}
